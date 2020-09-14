1d1bb26e08ab41e646ae0e15118993a46fa18a46
Andrea Scorcia <andrea.scorcia@gmail.com>
2020-09-14 10:33:21 +0200

    remove some dead code

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,13 +1,5 @@
 import axios from "axios";
 
-// export const extractIndexes = string => {
-//   const purifiedStr = string.match(/[0-9]/g);
-//   if (!purifiedStr) {
-//     return null;
-//   }
-//   return purifiedStr.map(item => +item);
-// };
-
 export const extractIndexes = string => {
   const { input } = string.match(/\d+/);
   if (!input) return null;
